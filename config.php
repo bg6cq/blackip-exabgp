bdf3f23bb648c6be8616ff179a2a5d3cbf79e3af
wallacemariadeandrade <wallacemariadeandrade@gmail.com>
2020-10-07 15:07:12 -0300

    Removed example credentials

--- config.php
+++ config.php
@@ -12,13 +12,13 @@ abstract class AuthenticationMethod
 $authentication_method = AuthenticationMethod::RADIUS; // Values are USTC_CAS, RADIUS
 
 // RADIUS parameters - leave it blank if you're not using RADIUS authentication
-$radius_server_ip = "10.3.4.34";
-$radius_shared_secret = "!@#k2!@#Telecom2018";
+$radius_server_ip = "";
+$radius_shared_secret = "";
 
 $routerip="210.45.230.89"; // Put your exabgp router IP address here
 $db_host = "localhost"; // Replace to your server host if needed
 $db_user = "blackip-exabgp"; // Replace to your desired database user
-$db_passwd = "blackip-exabgp@k2telecom"; // Put your database user password here
+$db_passwd = ""; // Put your database user password here
 $db_dbname = "blackip"; // You shudn't change this name
 
 // *** CONFIGURATON SECTION ****
